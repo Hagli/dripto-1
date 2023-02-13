ff89311cbb78afbe8a738ed205e5816fe14253a4
hagli <hagli2001@gmail.com>
2023-02-13 17:48:52 +0000

    Change app to a single page one.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import NavigationBar from './components/Navbar';
-import {Routes, Route, BrowserRouter} from 'react-router-dom';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/esm/Button';
 import Vigenere from './pages/vigenere';
 import AutoVigenere from './pages/auto-key-vigenere';
 import ExtVigenere from './pages/ext-vigenere';
 import Affine from './pages/affine';
 import Playfair from './pages/playfair';
 import Hill from './pages/hill';
+import { Container } from 'react-bootstrap';
 
-function App() {
-  return (
-    <BrowserRouter>
-    <NavigationBar />
-    <Routes>
-        <Route index element={<Vigenere/>} />
-        <Route path='/vigenere' exact element={<Vigenere/>} />
-        <Route path='/auto-key-vigenere' exact element={<AutoVigenere/>} />
-        <Route path='/ext-vigenere' exact element={<ExtVigenere/>} />
-        <Route path='/affine' exact element={<Affine/>} />
-        <Route path='/playfair' exact element={<Playfair/>} />
-        <Route path='/hill' exact element={<Hill/>} />
-        <Route path='*' element={<Vigenere/>} />
-    </Routes>
-    </BrowserRouter>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        visible : 1
+    }
+  }
+
+  render(){
+    return (
+      <div>
+        <Navbar bg='dark' variant='dark'>
+          <Container>
+            <Navbar.Brand>Dripto</Navbar.Brand>
+            <Nav className="me-auto">
+              <Button variant="dark" onClick={() => this.setState({visible: 1})}>
+                Vigenere
+              </Button>
+              <Button variant="dark" onClick={() => this.setState({visible: 2})}>
+                Auto-Key Vigenere
+              </Button>
+              <Button variant="dark" onClick={() => this.setState({visible: 3})}>
+                Extended Vigenere
+              </Button>
+              <Button variant="dark" onClick={() => this.setState({visible: 4})}>
+                Affine Cypher
+              </Button>
+              <Button variant="dark" onClick={() => this.setState({visible: 5})}>
+                Playfair Cypher
+              </Button>
+              <Button variant="dark" onClick={() => this.setState({visible: 6})}>
+                Hill Cypher
+              </Button>
+            </Nav>
+          </Container>
+        </Navbar>
+        
+        <Container>
+          {this.state.visible === 1 ? <Vigenere /> :
+          this.state.visible === 2 ? <AutoVigenere /> :
+          this.state.visible === 3 ? <ExtVigenere /> :
+          this.state.visible === 4 ? <Affine /> :
+          this.state.visible === 5 ? <Playfair /> :
+          this.state.visible === 6 ? <Hill /> :
+          null}
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
